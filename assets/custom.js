109cac58d4ea542d5f966569fa61f25a7710cf35
Metodi <shopov.metodi@gmail.com>
2020-11-25 01:49:40 +0200

    - added ingredients section - added drop down functionality - changed name of add to cart

--- assets/custom.js
+++ assets/custom.js
@@ -3,3 +3,13 @@
 /-----------------------------------------------------------------------------*/
 
 // Insert any custom theme js here...
+
+
+function drop_down() {
+    var x = document.getElementById("extra_info_btn");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
